b83a7b44b48ceeddc04a3cba3bc070438750bd7f
LamTran <49675955+TranVietlam798@users.noreply.github.com>
2021-10-22 16:04:20 +0700

    'setup'

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+//middleware
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+
+//routes
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'Hello'
+    })
+})
+
+//datebase
+
+import './config/database'
+
+
+//server listening
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is runing on port', PORT);
+
+})
